4613ec596e915edfd2d42855b803c98a3a020da7
Ernesto Pro <er_1991@live.com.mx>
2023-05-25 18:04:04 -0600

    videoHome

--- resources/views/blog/sections/video.blade.php
+++ resources/views/blog/sections/video.blade.php
@@ -4,7 +4,7 @@
         <div class="row h-100  justify-content-center align-content-center">
 
             <div class="video-wrap">
-                <div class="video">
+                <div class="video h-100">
                   {!! html_entity_decode($video->description, ENT_QUOTES | ENT_XML1, 'UTF-8') !!}
                 </div>
             </div>
